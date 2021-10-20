3fb7cd236457a1d7eb54194e14a623fb4825ae32
Omari Brown <Omaribrown16@gmail.com>
2021-10-20 06:36:06 -0500

    Created files

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1> Omari's Store </h1>
+    <p> ~Customer Satisfaction is Our Goal~</p>
+    <h3> Welcome!!! </h3>
+</header>
+
+
+
+
---
+++ orderform.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>MY Order Form</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=devicewidth, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="description" content="Omari's Order Form">
+        <meta name="author" content="Omari Brown">
+        <link rel="stylesheet" href="orderformstyles.css">
+        <script src="orderform.js"></script>
+    </head>
+    <body>
+        <!--<?php include 'header.php'; ?>-->
+        <header>
+            <h1> Omari's Store </h1>
+            <p> ~Customer Satisfaction is Our Goal~</p>
+            <h3> Welcome!!! </h3>
+        </header>
+        <main>
+            <h2>Homepage</h2>
+            <p>Please complete the form below to order your items.</p>
+        </main>
+        <footer>
+            <!--<?php include 'footer.php';?>-->
+            <p>Copyright &copy; 1999-" . date("Y") . " Omari Brown </p>
+        </footer>
+    </body>
+</html>
